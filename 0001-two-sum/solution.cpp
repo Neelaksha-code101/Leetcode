e3867a55c335577a1d34c2d0b403a39b8c853610
Neelaksha-code101 <277435772+Neelaksha-code101@users.noreply.github.com>
2026-08-12 18:23:09 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 15 MB (24.23%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
-                }
-            }
+       unordered_map<int,int> seen;
+
+       for(int i=0;i<nums.size();i++){
+          int needed=target-nums[i];
+
+          if(seen.count(needed)){
+            return{i,seen[needed]};
+          }
+          seen[nums[i]]=i;
         }
-        return {0};     
+       return{0};
     }
 };
